63bbd707132b16b0989ae2daa48fc2e141f76a8d
max <maxrider@mail.ru>
2016-11-06 13:27:39 +0300

    Initial 04

---
+++ 13/04/boot.s
@@ -0,0 +1,40 @@
+.code16
+
+.data
+signature:
+        .word 0xaa55
+
+.text
+
+msg:
+        .asciz "Hello, World!"
+
+.global _start
+_start:
+        cli
+        xorw %ax, %ax
+        movw %ax, %ds
+        
+        movw $0x3, %ax
+        int $0x10
+        
+        movw $0xb800, %dx
+        movw %dx, %ss
+        
+        movw $msg, %si
+        movw $0x2, %cx
+print_loop:
+        lodsb
+        or %al, %al
+        jz endless
+
+        movw %cx, %sp
+        
+        and $0x00ff, %ax
+        or $0x0f00, %ax
+        push %ax
+
+        add $0x2, %cx
+        jmp print_loop
+endless:
+        jmp endless
