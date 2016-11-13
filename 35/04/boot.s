1d65cd2e8294da90dddd14ef1fdfb681e6aa90d2
Sergey Voytovich <sergey.voytovich@jetbrains.com>
2016-11-13 14:35:13 +0100

    implement task #4

---
+++ 35/04/boot.s
@@ -0,0 +1,31 @@
+.code16                       # Assemble for 16-bit mode
+
+.data
+  .word   0xAA55              # Load sector 'marker'
+
+.text
+  hello_msg:
+  .asciz  "Hello world!"
+
+  green_clr:
+  .word   0x0002
+
+  .globl _start
+
+_start:
+  cli                         # BIOS enabled interrupts; disable
+  movw   $hello_msg, %si      # Load string address to si for lods
+
+write_string:
+  lodsb                       # Load next byte
+
+  or     %al, %al             # Break writing on null terminal
+  je     end
+
+  movb   $0x0E, %ah           # Move a character to vga
+  int    $0x10                # call BIOS video service interruption
+
+  jmp    write_string
+
+end:
+  jmp end
