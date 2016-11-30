954790d800a31eaf5695699ec9e9e4ef4bc05aac
Ilya Afanasev <ilya.orsk@gmail.com>
2016-11-30 22:44:56 +0300

    Change to direct access memory method

--- 01/04/hello_mbr.s
+++ 01/04/hello_mbr.s
@@ -8,21 +8,34 @@ mov $0x9000, %ax #Set up stack
 mov %ax, %ss     #Tell processor where stack is
 mov $0xFB00, %sp #Set stack offset
 lea  hello, %si
-jmp WriteString
+
+movw  $0xB800, %ax
+movw  %ax, %es
+
+mov   $0x07, %ah
 
 WriteString:
   lodsb                   # load byte at ds:si into al (advancing si)
   or     %al, %al         # test if character is 0 (end)
-  jz     cicle 			    # jump to end if 0.
+  jz     clean_init 			      # jump to end if clean.
 
-  mov    $0xE, %ah          # Subfunction 0xe of int 10h (video teletype output)
-  mov    $9, %bx            # Set bh (page nr) to 0, and bl (attribute) to white (9)
-  int    $0x10              # call BIOS interrupt.
-
+  movw   %ax, %es:(%di)
+  lea    2(%di), %di
   jmp    WriteString      # Repeat for next character.
 
+clean_init:
+  mov $20, %ax
+  mov $0x300, %cx
+clean_loop:
+  dec %cx
+  or %cx, %cx
+  jz cicle
+  movw   %ax, %es:(%di)
+  lea    2(%di), %di
+  jmp clean_loop
+
 cicle:
-jmp cicle   #And so on and so on
+  jmp cicle   #And so on and so on
 
 hello:                  
   .asciz "Hello, World!"
