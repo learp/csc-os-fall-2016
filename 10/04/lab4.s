7414e7fea5c316130ccd8572fcebd9e97930d8ba
PaGul <zagerpaul@gmail.com>
2016-12-12 20:00:24 +0300

    Use video buffer direct

--- 10/04/lab4.s
+++ 10/04/lab4.s
@@ -1,30 +1,47 @@
 .code16
-.global _start
-.data 
+# MBR number
+.data
 	.word 0xaa55
-.text 
+.global _start
+.text
 	hello_str: .asciz "Hello, World!"
 _start:
 # clear interrupt and direction flags
 	cli
 	cld
-# clear ax
-	xorw %ax, %ax
 # set to si the address of the hello_str's beginning
 	movw $hello_str, %si
-	
+
+# write video buffer address to ES:DI, DI should be 0 (first symbol on the console)
+	movw $0xB800, %bx
+	movw %bx, %es
+	movw $0x00, %di	
+
+# choose the letter's color (white)
+	movb $0x07, %ah
+
 _print_loop:
 # load the byte from the string to al
 	lodsb
 # check if the string ends
 	or %al, %al
 # stop the loop
-	jz _end_print_loop
-# print the letter
-	movb $0x0e, %ah
-	int $0x10
+	jz _clear_to_the_end
+
+# print the letter (al contains the letter and ah contains the color)
+	stosw
 # continue the print_loop
 	jmp _print_loop
-# infinite loop of end
+
+_clear_to_the_end:
+
+# write the empty symbol to ax
+	movw $0x0000, %ax
+# print symbol (80*25-13) times, because hello_str length equals 13
+	movw $0x7c3, %cx
+	rep stosw
+
+# infinite end loop
 _end_print_loop:
 	jmp _end_print_loop
+
